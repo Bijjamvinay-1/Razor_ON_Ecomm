167e4fa21af7876fddde6424f206890da926a5a8
Bijjamvinay-1 <vinaybijjam7771@gmail.com>
2023-07-27 17:32:57 +0530

    Add project files.

---
+++ Razor_ON_Ecomm/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Razor_ON_Ecomm.Model;
+
+namespace Razor_ON_Ecomm.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
